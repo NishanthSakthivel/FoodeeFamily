08c5a508e664d40e32a41397248f3212ab7fd54e
Nishanth <rsvnishanth95@outlook.com>
2021-09-06 19:44:27 +0530

    third deploy without warniings

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BrandImg from "../Assets/Brand.png";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 
 const NavBar = () => {
